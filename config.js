c546d0d36aacd1cc61998e52ca3288001e23f243
iabsensimaster <iabsensimaster@gmail.com>
2025-10-02 15:36:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js - konfigurasi aplikasi i-Armada
 // Update `API_URL` dengan Web App URL dari Apps Script (lihat instruksi deploy)
-const API_URL = 'https://script.google.com/macros/s/AKfycbwHc-4WL9td9oNGb7Um_oARpPrmHrgXi-fGdAH65Hp_-ffFPolxhyYkrGqxQwhpy3D7/exec'; // <-- ganti
+const API_URL = 'https://script.google.com/macros/s/AKfycbwsRyTG3-2b8oImwWSeh0VFm2rSBpA2W6VWCE7yq68k9oHu-Fo06uoDNfgKgj5djLUW/exec'; // <-- ganti
 // Optional: fallback id_pt / id_spreadsheet stored in localStorage by UI ketika user pilih PT
